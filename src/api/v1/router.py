827a516ae0398a1aa262af814498fab47436bba1
asd <macintoshkrug@gmail.com>
2024-05-21 00:17:16 +0300

    refa

--- src/api/v1/router.py
+++ src/api/v1/router.py
@@ -1,38 +1,21 @@
-from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse, StreamingResponse
-from fastapi.templating import Jinja2Templates
+from fastapi import APIRouter, Request, HTTPException
+from fastapi.responses import StreamingResponse
 
-import  random, string
-from pathlib import Path
+IMAGE = '/app/src/static/img.gif'
 
-IMAGE_FILE = 'img.gif'
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-templates = Jinja2Templates(directory=str(Path(BASE_DIR, 'templates')))
 router = APIRouter()
-
-
-@router.get("/asdasdgenerate", response_class=HTMLResponse)
-def generate(request: Request):
-    return templates.TemplateResponse(
-        request=request, name="index.html", context={ "id": f"/static/pictures/{randomword(57)}.gif" }
-    )
 
 @router.get("/pictures/{id}")
 def pictures(id: str, request: Request):
-    
     ip = request.headers.get('X-Forwarded-For', None)
 
     if not ip:
-        return None
+        raise HTTPException(status_code=404, detail="Item not found")
 
     print(f"new_ip: {ip} ({id})")
     def iterfile():
-        with open(f"{str(Path(BASE_DIR, 'static', IMAGE_FILE))}", mode="rb") as file_like:
+        with open(IMAGE, mode="rb") as file_like:
             yield from file_like
 
     return StreamingResponse(iterfile(), media_type="image/gif")
 
-def randomword(length):
-   letters = string.ascii_lowercase
-   return ''.join(random.choice(letters) for i in range(length))
--- src/main.py
+++ src/main.py
@@ -1,7 +1,65 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter, HTTPException, Request, Response
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
-from .api.v1.router import router as router_v1
+import re, base64, random, string
+
+from .api.v1.router import router as trackers
 
 app = FastAPI()
+router = APIRouter()
+templates = Jinja2Templates(directory='/app/src/templates') # container path
 
-app.include_router(router_v1, prefix="/static")
+app.include_router(trackers, prefix="/static")
+
+fake_headers = {
+    "X-Powered-By": "PHP 8.1.1",
+    "Set-Cookie": "PHPSESSID=aWxvdmV5b3U=",
+    "Content-Type": "text/html",
+}
+
+@router.get("/asdasdgenerate", response_class=HTMLResponse)
+def generate(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={ "id": f"/static/pictures/{randomword(57)}.gif" }
+    )
+
+@router.get("/{_:path}", response_class=HTMLResponse)
+def index():
+    return HTMLResponse(
+        headers=fake_headers,
+        content="<body><h1>Not found</h1></body>",
+        media_type="text/html",
+        status_code=404
+    )
+
+app.include_router(router)
+
+
+@app.middleware("http")
+async def filter_bots(request: Request, call_next):
+    ua = request.headers.get('User-Agent', None)
+
+    if is_ua_in_blocklist(ua):
+        return Response(
+            headers=fake_headers,
+            status_code=403,
+        )
+
+    return await call_next(request)
+
+UA_TG = re.compile('telegram', re.IGNORECASE)
+UA_BOT = re.compile('bot', re.IGNORECASE)
+
+def is_ua_in_blocklist(ua):
+    if not ua:
+        return True
+    if UA_TG.search(ua):
+       return True 
+    if UA_BOT.search(ua):
+        return True
+    return False
+
+def randomword(length):
+   letters = string.ascii_lowercase
+   return base64.b64encode(bytes(''.join(random.choice(letters) for i in range(length)), 'utf-8')).decode("utf-8")
